cf0d2a92353b1530a386a399804ed3dde08d5cdd
Bo Morin <morinbe@gmail.com>
2020-12-10 16:15:07 -0500

    heroku it up

---
+++ project/settings_production.py
@@ -0,0 +1,12 @@
+import os
+from project.settings import *  # noqa
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(default=os.environ['DATABASE_URL'])
+
+DEBUG = True
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+ALLOWED_HOSTS = ['aytocalc.herokuapp.com', 'herokuapp.com',]
